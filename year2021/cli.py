a646b877c244e7752016b8f056372690b6c894b0
ecurtin2 <ecurtin2@illinois.edu>
2021-12-05 13:46:33 -0600

    d5

--- year2021/cli.py
+++ year2021/cli.py
@@ -3,6 +3,7 @@ from sys import argv
 from importlib import import_module
 from inspect import signature
 from pathlib import Path
+from timeit import default_timer as timer
 
 
 def parse_input(to, s: str):
@@ -17,23 +18,48 @@ def parse_input(to, s: str):
     raise ValueError(f"Can't parse to type {to}")
 
 
+def run(day: int, part: int):
+    mod = import_module(f"d{day}")
+    func = getattr(mod, f"p{part}")
+    sig = signature(func)
+
+    start = timer()
+    if sig.parameters:
+        input_type = list(sig.parameters.values())[0].annotation
+        input_file = Path(f"data/d{day}p{part}.txt")
+
+        # Fallback on part 1 since often reused
+        if not input_file.is_file():
+            input_file = Path(f"data/d{day}p1.txt")
+
+        inputs = parse_input(input_type, input_file.read_text())
+        result = func(inputs)
+    else:
+        result = func()
+
+    end = timer()
+    return result, end - start
+
+
 day, part = argv[1], argv[2]
-print(f"Running Day {day} part {part}")
-
-mod = import_module(f"d{day}")
-func = getattr(mod, f"p{part}")
-sig = signature(func)
-if sig.parameters:
-    input_type = list(sig.parameters.values())[0].annotation
-    input_file = Path(f"data/d{day}p{part}.txt")
-
-    # Fallback on part 1 since often reused
-    if not input_file.is_file():
-        input_file = Path(f"data/d{day}p1.txt")
-
-    inputs = parse_input(input_type, input_file.read_text())
-    result = func(inputs)
+if day == "x":
+    days = sorted(int(p.stem[1:]) for p in Path(__file__).parent.glob("d*.py"))
 else:
-    result = func()
+    days = [int(day)]
+if part == "x":
+    parts = [1, 2]
+else:
+    parts = [int(part)]
 
-print(result)
+print(" Day | Part | Duration | Answer ")
+print("-----|------|----------|--------")
+
+durations = []
+for day in days:
+    for part in parts:
+        print(f" {day:2}  |  {part}   |", end=" ")
+        result, duration = run(day, part)
+        durations.append(duration)
+        print(f" {duration:6.4f}  | {result}")
+
+print(f"\n Total duration for all parts: {sum(durations):6.4f}")
---
+++ year2021/d5.py
@@ -0,0 +1,62 @@
+import pytest
+import numpy as np
+
+
+def draw_line(ary, p1, p2):
+    if p1[0] == p2[0]:  # special case for vertical
+        y_min = min(p1[1], p2[1])
+        y_max = max(p1[1], p2[1]) + 1
+        ary[y_min:y_max, p1[0]] += 1
+        return
+
+    if p2[0] < p1[0]:
+        p1, p2 = p2, p1
+    slope = (p2[1] - p1[1]) / (p2[0] - p1[0])
+    xs = np.arange(p1[0], p2[0] + 1)
+    ys = (p1[1] + slope * np.arange(xs.shape[0])).astype(np.int32)
+    ary[ys, xs] += 1
+
+
+def solve(inputs, hv_only: bool) -> int:
+    lines = [[[int(x) for x in s.split(",")] for s in line.split("->")] for line in inputs.splitlines()]
+    if hv_only:
+        lines = np.array([l for l in lines if l[0][0] == l[1][0] or l[0][1] == l[1][1]])
+    else:
+        lines = np.array(lines)
+
+    max_x = np.amax(lines[:, 0, :])
+    max_y = np.amax(lines[:, 1, :])
+    space = np.zeros((max_x + 1, max_y + 1), dtype=np.int32)
+    for p1, p2 in lines:
+        draw_line(space, p1, p2)
+    return np.count_nonzero(space >= 2)
+
+
+def p1(inputs: str) -> int:
+    return solve(inputs, hv_only=True)
+
+
+def p2(inputs: str) -> int:
+    return solve(inputs, hv_only=False)
+
+
+@pytest.fixture()
+def example():
+    return """0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2"""
+
+
+def test_p1(example):
+    assert p1(example) == 5
+
+
+def test_p2(example):
+    assert p2(example) == 12
